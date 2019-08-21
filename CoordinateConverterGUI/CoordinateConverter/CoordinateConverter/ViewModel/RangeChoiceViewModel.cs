2c5166fc53fa25e28ab41a8e2d22b664c4e43bb1
Pisarb <33935576+Pisarb@users.noreply.github.com>
2019-08-21 12:40:39 +0300

    asd

--- CoordinateConverterGUI/CoordinateConverter/CoordinateConverter/ViewModel/RangeChoiceViewModel.cs
+++ CoordinateConverterGUI/CoordinateConverter/CoordinateConverter/ViewModel/RangeChoiceViewModel.cs
@@ -1,20 +1,25 @@
 ﻿using DevExpress.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace CoordinateConverter.ViewModel
 {
-    class RangeChoiceViewModel
+    class RangeChoiceViewModel : IDataErrorInfo
     {
 
         private readonly UICommand okCommand;
 
+
         private string first;
         private string last;
+        private bool errorFirst;
+        private bool errorLast;
 
         public string First
         {
@@ -22,6 +27,8 @@ namespace CoordinateConverter.ViewModel
             set
             {
                 first = value;
+                first = first?.ToUpper();
+
             }
 
         }
@@ -31,6 +38,53 @@ namespace CoordinateConverter.ViewModel
             set
             {
                 last = value;
+                last = last?.ToUpper();
+            }
+        }
+
+        public string Error => null;
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = String.Empty;
+                Regex excelCellFormat = new Regex(@"^[A-Z]+[0-9]+$");
+                if (columnName == "First")
+                {
+                    if (First != string.Empty)
+                    {
+
+                        if (!excelCellFormat.IsMatch(First))
+                        {
+                            error = "Неверный формат";
+                            this.errorFirst = true;
+                        }
+                        else
+                            this.errorFirst = false;
+                    }
+                    else
+                        this.errorFirst = true;
+                }
+                if (columnName == "Last")
+                {
+                    if (Last != string.Empty)
+                    {
+                        Last = Last.ToUpper();
+                        if (!excelCellFormat.IsMatch(Last))
+                        {
+                            error = "Неверный формат";
+                            this.errorLast = true;
+                        }
+                        else
+                            this.errorLast = false;
+
+                    }
+                    else
+                        this.errorLast = true;
+                }
+                
+                return error;
             }
         }
 
@@ -43,9 +97,20 @@ namespace CoordinateConverter.ViewModel
                 Command = new DelegateCommand(() =>
                 {
 
+                }, 
+                () =>
+                {
+                    return !errorFirst && !errorLast;
                 })
             };
+            first = string.Empty;
+            last = string.Empty;
         }
+        
+        //private bool OKCanExecute()
+        //{
+
+        //}
 
         public IEnumerable<UICommand> GetCommands()
         {
