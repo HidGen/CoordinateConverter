66d137f49e214e6d0d723537fc317fd6fc11bf02
Pisarb <33935576+Pisarb@users.noreply.github.com>
2019-08-28 11:00:01 +0300

    ne hui

---
+++ CoordinateConverterGUI/CoordinateConverter/CoordinateConverter/NewItemRowBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using CoordinateConverter.Model;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.UI.Interactivity;
+using DevExpress.Xpf.Grid;
+
+namespace CoordinateConverter
+{
+    public class NewItemRowBehavior : Behavior<TableView>
+    {
+        public ICommand NewItemRowUpdated
+        {
+            get { return (ICommand)GetValue(NewItemRowUpdatedProperty); }
+            set { SetValue(NewItemRowUpdatedProperty, value); }
+        }
+        public static readonly DependencyProperty NewItemRowUpdatedProperty =
+            DependencyProperty.Register("NewItemRowUpdated", typeof(ICommand), typeof(NewItemRowBehavior), new PropertyMetadata(null));
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.RowUpdated += OnRowUpdated;
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+        }
+
+        void OnRowUpdated(object sender, RowEventArgs e)
+        {
+            
+            OnRowUpdated(e);
+        }
+
+        protected virtual void OnRowUpdated(RowEventArgs e)
+        {
+            try
+            {
+                if (e.RowHandle == GridControl.NewItemRowHandle && NewItemRowUpdated != null && NewItemRowUpdated.CanExecute(e.Row))
+                    NewItemRowUpdated.Execute(e.Row);
+            }
+            catch { }
+        }
+    }
+}
---
+++ CoordinateConverterGUI/CoordinateConverter/CoordinateConverter/ViewModel/EnumConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Windows.Data;
+using System.Windows;
+
+namespace CoordinateConverter.ViewModel
+{
+
+    public class EnumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (value == null)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+            else
+            {
+                return GetDescription((Enum)value);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public static string GetDescription(Enum en)
+        {
+            Type type = en.GetType();
+            MemberInfo[] memInfo = type.GetMember(en.ToString());
+            if (memInfo != null && memInfo.Length > 0)
+            {
+                object[] attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (attrs != null && attrs.Length > 0)
+                {
+                    return ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+            return en.ToString();
+        }
+    }
+
+}
