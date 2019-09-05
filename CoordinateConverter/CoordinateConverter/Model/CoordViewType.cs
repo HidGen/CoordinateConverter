66a186835070257b3bc07799e54dcce2cee6033c
HidGen <hiddengenesis6@gmail.com>
2019-09-05 16:11:49 +0300

    Mid_Final

---
+++ CoordinateConverter/CoordinateConverter/Model/CoordViewType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace CoordinateConverter.ViewModel
+{
+    [TypeConverter(typeof(EnumToStringConverter))]
+    public enum CoordViewType
+    {
+
+        [Description("Десятичные градусы")]
+        Decimal,
+        [Description("Градусы, минуты, секунды")]
+        MinSec
+    }
+
+
+}
---
+++ CoordinateConverter/CoordinateConverter/Model/CoordinateType.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace CoordinateConverter.ViewModel
+{
+    [TypeConverter(typeof(EnumToStringConverter))]
+    public enum CoordinateType
+    {
+        [Description("МСК-46 зона 1")]
+        MSK461,
+        [Description("МСК-46 зона 2")]
+        MSK462,
+        [Description("СК-42")]
+        SK42,
+        [Description("СК-63")]
+        SK63
+    }
+
+
+}
